c8820ef93533be0c3f459f0bcb72bfd3de7de2b0
superfaz <16510828+superfaz@users.noreply.github.com>
2024-09-28 14:12:49 +0200

    Code review

--- tests/Block.test.ts
+++ tests/Block.test.ts
@@ -9,13 +9,13 @@ class TestError extends Error {
 }
 
 describe("Block", () => {
-  test(".succeed()", async () => {
+  test("succeed()", async () => {
     const actual: PromisedResult<string> = Block.succeed("Hello, World!");
 
     expect(await actual).toEqual({ success: true, data: "Hello, World!" });
   });
 
-  test(".fail()", async () => {
+  test("fail()", async () => {
     const actual: PromisedResult<never, TestError> = Block.fail(
       new TestError(),
     );
@@ -26,26 +26,28 @@ describe("Block", () => {
     });
   });
 
-  test("convert() success", async () => {
-    const actual = Block.convert({
-      try: () => Promise.resolve("result"),
-      catch: () => new TestError(),
+  describe("convert()", () => {
+    test("success", async () => {
+      const actual = Block.convert({
+        try: () => Promise.resolve("result"),
+        catch: () => new TestError(),
+      });
+
+      expect(await actual).toEqual({ success: true, data: "result" });
     });
 
-    expect(await actual).toEqual({ success: true, data: "result" });
-  });
+    test("fail", async () => {
+      const actual = Block.convert({
+        try: async () => {
+          throw new TestError();
+        },
+        catch: (e) => (e instanceof Error ? e : new TestError()),
+      });
 
-  test("convert() fail", async () => {
-    const actual = Block.convert({
-      try: async () => {
-        throw new TestError();
-      },
-      catch: (e) => (e instanceof Error ? e : new TestError()),
-    });
-
-    expect(await actual).toEqual({
-      success: false,
-      error: new TestError("error"),
+      expect(await actual).toEqual({
+        success: false,
+        error: new TestError("error"),
+      });
     });
   });
 });
